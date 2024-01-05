955bdf1b9f9affffd9ad6e21ffe5a598da8314ca
Amit Kumar <amit90510@gmail.com>
2024-01-05 14:20:25 +0530

    initial setup

---
+++ DBS/AddDBUser.sql
@@ -0,0 +1,8 @@
+﻿USE [ExamOn_Dummy]
+GO
+
+/****** Object:  User [examon]    Script Date: 05-01-2024 02:16:45 PM ******/
+CREATE USER [examon] FOR LOGIN [examon] WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+
---
+++ DBS/AddServerLogin.sql
@@ -0,0 +1,18 @@
+﻿USE [master]
+GO
+
+/* For security reasons the login is created disabled and with a random password. */
+/****** Object:  Login [examon]    Script Date: 05-01-2024 02:14:14 PM ******/
+CREATE LOGIN [examon] WITH PASSWORD=N'Welcome@90510', DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
+GO
+
+ALTER LOGIN [examon] DISABLE
+GO
+
+ALTER SERVER ROLE [sysadmin] ADD MEMBER [examon]
+GO
+
+ALTER SERVER ROLE [setupadmin] ADD MEMBER [examon]
+GO
+
+
