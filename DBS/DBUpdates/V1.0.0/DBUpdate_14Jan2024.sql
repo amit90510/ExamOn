bd104378a02ab8471ecbad4d19ac2407523817b1
Amit Kumar <amit90510@gmail.com>
2024-01-14 16:36:42 +0530

    Test

---
+++ DBS/DBUpdates/V1.0.0/DBUpdate_14Jan2024.sql
@@ -0,0 +1,42 @@
+﻿USE [ExamOn_Dummy]
+GO
+
+ALTER TABLE [dbo].[tblLoginHistory] DROP CONSTRAINT [FK_tblLoginHistory_tbllogin]
+GO
+
+/****** Object:  Table [dbo].[tblLoginHistory]    Script Date: 14-01-2024 04:33:27 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblLoginHistory]') AND type in (N'U'))
+DROP TABLE [dbo].[tblLoginHistory]
+GO
+
+/****** Object:  Table [dbo].[tblLoginHistory]    Script Date: 14-01-2024 04:33:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblLoginHistory](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[UserName] [varchar](50) NOT NULL,
+	[Ip] [varchar](50) NOT NULL,
+	[Browser] [varchar](100) NOT NULL,
+	[LoginDate] [date] NOT NULL,
+ CONSTRAINT [PK_tblLoginHistory_1] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[tblLoginHistory]  WITH CHECK ADD  CONSTRAINT [FK_tblLoginHistory_tbllogin] FOREIGN KEY([UserName])
+REFERENCES [dbo].[tbllogin] ([UserName])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[tblLoginHistory] CHECK CONSTRAINT [FK_tblLoginHistory_tbllogin]
+GO
+
+create nonclustered index idx_Noncluserted on tblloginhistory(UserName)
+GO
