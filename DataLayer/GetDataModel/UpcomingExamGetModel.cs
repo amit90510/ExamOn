86fcd25daff36d705f9ac27a42519166e6d2fb04
Amit Kumar <amit90510@gmail.com>
2024-01-27 22:33:41 +0530

    test

---
+++ DataLayer/GetDataModel/UpcomingExamGetModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamOn.DataLayer.GetDataModel
+{
+    public class UpcomingExamGetModel
+    {
+        public string ExamName { get; set; }
+        public DateTime StartExam { get; set; }
+        public DateTime updatedOn { get; set; }
+        public DateTime EntryAllowedTill { get; set; }
+
+        public int SectionCount { get; set; }
+    }
+}
---
+++ DataLayer/GetDataModel/UserShiftAssociation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamOn.DataLayer.GetDataModel
+{
+    public class UserShiftAssociation
+    {
+        public string ClassName { get; set; }
+        public string Batch { get; set; }
+        public string Shift { get; set; }
+    }
+}
