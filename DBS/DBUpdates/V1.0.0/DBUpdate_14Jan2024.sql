70aa170dceb02146cfd1ef3b8dc3e4ad41d0d46a
Amit Kumar <amit90510@gmail.com>
2024-01-28 13:24:42 +0530

    test

--- DBS/DBUpdates/V1.0.0/DBUpdate_14Jan2024.sql
+++ DBS/DBUpdates/V1.0.0/DBUpdate_14Jan2024.sql
@@ -174,3 +174,55 @@ GO
 create nonclustered index idx_tblusershift_index on tbluserShift(userId) include(shiftId)
 
 GO
+
+ALTER TABLE [dbo].[tbluserProfile] DROP CONSTRAINT [FK_tbluserProfile_tbllogin1]
+GO
+
+ALTER TABLE [dbo].[tbluserProfile] DROP CONSTRAINT [FK_tbluserProfile_tbllogin]
+GO
+
+/****** Object:  Table [dbo].[tbluserProfile]    Script Date: 28-01-2024 01:23:46 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tbluserProfile]') AND type in (N'U'))
+DROP TABLE [dbo].[tbluserProfile]
+GO
+
+/****** Object:  Table [dbo].[tbluserProfile]    Script Date: 28-01-2024 01:23:46 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tbluserProfile](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[UserName] [varchar](50) NOT NULL,
+	[address] [varchar](50) NULL,
+	[City] [varchar](50) NULL,
+	[State] [varchar](50) NULL,
+	[Shift] [bigint] NULL,
+	[RealName] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_tbluserProfile] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[tbluserProfile]  WITH CHECK ADD  CONSTRAINT [FK_tbluserProfile_tbllogin] FOREIGN KEY([Shift])
+REFERENCES [dbo].[tblshift] ([id])
+ON UPDATE SET NULL
+ON DELETE SET NULL
+GO
+
+ALTER TABLE [dbo].[tbluserProfile] CHECK CONSTRAINT [FK_tbluserProfile_tbllogin]
+GO
+
+ALTER TABLE [dbo].[tbluserProfile]  WITH CHECK ADD  CONSTRAINT [FK_tbluserProfile_tbllogin1] FOREIGN KEY([UserName])
+REFERENCES [dbo].[tbllogin] ([UserName])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[tbluserProfile] CHECK CONSTRAINT [FK_tbluserProfile_tbllogin1]
+GO
+
+
