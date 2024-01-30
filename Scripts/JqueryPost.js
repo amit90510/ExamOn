08edf465a7baf7b457a3897d186fc884623c7d05
Amit Kumar <amit90510@gmail.com>
2024-01-30 10:46:40 +0530

    Test

--- Scripts/JqueryPost.js
+++ Scripts/JqueryPost.js
@@ -36,3 +36,34 @@ function SwalFire(title, text, icon, footer, thenCallback, html='') {
         thenCallback();
     });
 }
+
+//below 2 function is for saving date and datetime to sql DB
+function toSaveSqlDBDateFormat(dt, sourceformat = 'dd-mm-yyyy', targetformat = 'YYYY-MM-DD') {
+    if (dt) {
+        return moment(dt, sourceformat).format(targetformat)
+    }
+}
+
+function toSaveSqlDBDateTimeFormat(dtime, sourceformat = 'dd/mm/yyyy hh:mm a', targetdtformat = 'YYYY-MM-DD', sourceTimeformat = 'hh:mm a', targetTimeformat = 'THH:mm:ss') {
+    if (dtime) {
+        let splitTime = dtime.split(' ');
+        let convertedDate = moment(dtime, sourceformat).format(targetdtformat);
+        let convertedTime = moment(splitTime[1] + ' ' + splitTime[2], sourceTimeformat).format(targetTimeformat);
+
+        return convertedDate + convertedTime;
+    }
+}
+
+
+//below 2 function is for showing date and datetime from sql DB to UI
+function toShowSqlDateinUI(dt, sourceformat = 'YYYY-MM-DD', targetformat = 'dd-mm-yyyy') {
+    if (dt) {
+        return toSaveSqlDBDateFormat(dt, sourceformat, targetformat);
+    }
+}
+
+function toShowSqlDatetimeinUI(dtime, sourceFormat = 'YYYY-MM-DDTHH:mm:ss', targetdateFormat = 'DD-MM-YYYY hh:mm a') {
+    if (dtime) {
+        return toSaveSqlDBDateFormat(dtime, sourceFormat, targetdateFormat);
+    }
+}
