c40b0d6101a800f33862ca817ca1a99d8070410b
Amit Kumar <amit90510@gmail.com>
2024-02-12 16:08:07 +0530

    test

--- Controllers/InstructorTeacherDashboardController.cs
+++ Controllers/InstructorTeacherDashboardController.cs
@@ -1,7 +1,11 @@
 ﻿using ExamOn.Authorize;
+using ExamOn.DataLayer;
+using ExamOn.Models;
+using ExamOn.ServiceLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,6 +17,20 @@ namespace ExamOn.Controllers
         public ActionResult Go()
         {
             return View("Index");
+        }
+
+        [AuthorizeAction]
+        [ForgeryTokenAuthorize]
+        public async Task<JsonResult> GetAssocaitionHistory()
+        {
+            JsonData jsonData = new JsonData();
+            var tenantData = DapperService.GetDapperData<tbltenant>("select TenantName, TenantMobile, TenantEmail, SubscriptionEndDate from tblTenant where Id = @id", new { id = AuthorizeService.GetDBToken(HttpContext.User.Identity.Name) });
+            if (tenantData != null && tenantData.Any())
+            {
+                jsonData.StatusCode = 1;
+                jsonData.Data = tenantData.ToList();
+            }
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
         }
     }
 }
