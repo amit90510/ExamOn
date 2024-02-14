ec7952ced50647fb1a98769aff8acd68bfc07bd5
Amit Kumar <amit90510@gmail.com>
2024-02-14 10:07:26 +0530

    test

---
+++ Controllers/StudentSignupController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace ExamOn.Controllers
+{
+    public class StudentSignupController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Go()
+        {
+            ViewBag.Version = WebConfigurationManager.AppSettings["ExamOnVersion"];
+            return View("Index");
+        }
+    }
+}
