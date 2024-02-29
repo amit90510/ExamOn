7f727ec3601a99eb07011c5a803be6ba75deeb51
Amit Kumar <amit90510@gmail.com>
2024-02-29 13:48:12 +0530

    test

---
+++ Scripts/ExamOn_Util_common.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    try {
+        const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        tooltipTriggerList.forEach(tooltipTriggerEl => {
+            if (tooltipTriggerEl.getAttribute("examon_tooltip")) {
+                new bootstrap.Tooltip(tooltipTriggerEl, { title: tooltipTriggerEl.getAttribute("examon_tooltip"), placement: "top" });
+            }
+        });
+    } catch{
+    }
+});
